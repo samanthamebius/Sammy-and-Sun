808c88548216398a77af0bee2cac2d248b249c01
Christine <csun325@aucklanduni.ac.nz>
2022-08-09 10:54:07 +1200

    formatted Clutch and ToteBag class

--- app/src/main/java/com/example/se306_project1/models/ToteBag.java
+++ app/src/main/java/com/example/se306_project1/models/ToteBag.java
@@ -2,12 +2,37 @@ package com.example.se306_project1.models;
 
 import com.google.firebase.database.Exclude;
 
+import java.util.ArrayList;
+
 public class ToteBag extends Product{
 
     private int categoryID = 3;
+    private int productID;
+    private Double productPrice;
+    private String productLongName;
+    private String productShortName;
 
-    public ToteBag(){
+    private ArrayList<String> productImages;
+    private String productDescription;
+    private String productDetails;
+    private String productCare;
 
+    private String brandName;
+    private String productColourType;
+    private Boolean productIsFavourite = Boolean.FALSE;
+    private int productCountVisit = 0;
+
+    public ToteBag(int productID, Double productPrice, String productLongName, String productShortName, ArrayList<String> productImages, String productDescription, String productDetails, String productCare){
+        this.productID = productID;
+        this.productPrice = productPrice;
+        this.productLongName = productLongName;
+        this.productShortName = productShortName;
+        this.productImages = productImages;
+        this.productDescription = productDescription;
+        this.productDetails = productDetails;
+        this.productCare = productCare;
+        this.brandName = getBrandName();
+        this.productColourType = getProductColourType();
     }
 
     @Override
