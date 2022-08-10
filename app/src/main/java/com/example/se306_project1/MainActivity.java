abb3e29a8b0c447f2ab433189cdd1d26a53126b3
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-10 20:50:52 +1200

    update product id's
    changed ids to be integers that were unique across all categories of bag

--- app/src/main/java/com/example/se306_project1/MainActivity.java
+++ app/src/main/java/com/example/se306_project1/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.se306_project1.data.ClutchesDataProvider;
+import com.example.se306_project1.data.CrossBodyBagsDataProvider;
+import com.example.se306_project1.data.ToteBagsDataProvider;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ClutchesDataProvider.addClutchestoFirestore();
+        ToteBagsDataProvider.addToteBagstoFirestore();
+        CrossBodyBagsDataProvider.addCrossBodyBagstoFirestore();
     }
 }
