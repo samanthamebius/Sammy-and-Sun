1a18060ac1f6278ade846dc0ffefa3bbb03a64a9
Csun325 <Csun325@aucklanduni.ac.nz>
2022-08-10 16:09:51 +1200

    implemented method to get collection details from firebase

--- app/src/main/java/com/example/se306_project1/repository/ProductRepository.java
+++ app/src/main/java/com/example/se306_project1/repository/ProductRepository.java
@@ -2,21 +2,15 @@ package com.example.se306_project1.repository;
 
 
 import android.util.Log;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.se306_project1.models.Product;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
-
-
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -38,29 +32,37 @@ public class ProductRepository {
     }
 
     // method to make database queries
-    public MutableLiveData<List<Product>> getProducts() {
-        // retrieve data from web service and add to productsDataSet
+//    public MutableLiveData<List<Product>> getProducts() {
+//        // retrieve data from web service and add to productsDataSet
+//
+//        MutableLiveData<List<Product>> data = new MutableLiveData<>();
+//        data.setValue(productsDataSet);
+//        return data;
+//    }
 
-        MutableLiveData<List<Product>> data = new MutableLiveData<>();
-        data.setValue(productsDataSet);
-        return data;
-    }
-
-    private void getAllProducts(){
-        dref = FirebaseDatabase.getInstance().getReference();
-        dref.child("clutches").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+    public void getAllProducts(){
+        CollectionReference colref = FirebaseFirestore.getInstance().collection("clutches");
+        colref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                // unsuccessful load
                 if(!task.isSuccessful()){
-                    Log.d("firebase", "Error getting data", task.getException());
+                    Log.d("firebase", "Error getting data!", task.getException());
                 }
                 else{
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                    task.getResult(); // pass through to productDataSet
+                    // successful load
+                    Log.d("firebase", String.valueOf(task.getResult()));
+                    List<DocumentSnapshot> snap = task.getResult().getDocuments();
+                    for(DocumentSnapshot singleBag : snap){
+                        String productLongName = singleBag.get("productLongName").toString();
+                        String productShortName = singleBag.get("productShortName").toString();
+                        System.out.println(productLongName);
+                    }
                 }
             }
         });
     }
+
 
 
 }
