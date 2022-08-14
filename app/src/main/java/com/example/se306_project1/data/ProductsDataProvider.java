2ad4b7b8b69ab922f6cf9781d4370077e5b97a2b
Christine <csun325@aucklanduni.ac.nz>
2022-08-14 15:27:20 +1200

    fixed build errors

--- app/src/main/java/com/example/se306_project1/data/ProductsDataProvider.java
+++ app/src/main/java/com/example/se306_project1/data/ProductsDataProvider.java
@@ -89,8 +89,8 @@ public class ProductsDataProvider {
             String productDescription = descriptions.get(key);
             String productDetails = details.get(key);
             String productCare = productCares.get(key);
-            String brandName = brands.get(key);
-            String productColourType = colourTypes.get(key);
+            Brand brandName = Brand.valueOf(brands.get(key));
+            ColourType productColourType = ColourType.valueOf(colourTypes.get(key)) ;
 
 
             Product c = new Product(productID, categoryID, productPrice, productLongName, productShortName, brandName, productDescription, productDetails, productCare, productColourType, productCountVisit, isFavourite,productImages);
