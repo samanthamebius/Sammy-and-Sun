4f4b0d83c7a6fdb1cfd5092ff93f03146011fb1a
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-16 11:35:51 +1200

    implemented GetFavourites class

--- app/src/main/java/com/example/se306_project1/repository/IFavouritesRepository.java
+++ app/src/main/java/com/example/se306_project1/repository/IFavouritesRepository.java
@@ -1,11 +1,15 @@
 package com.example.se306_project1.repository;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
+import com.example.se306_project1.models.Category;
 import com.example.se306_project1.models.Product;
 
 import java.util.List;
 
 public interface IFavouritesRepository {
-    public LiveData<List<Product>> getFavourites();
+
+    LiveData<List<Product>> getFavourites();
+
 }
