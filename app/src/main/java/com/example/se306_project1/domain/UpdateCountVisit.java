bc505a8b8e018fe52df92674c8aa00cdcc27cf1f
arim402 <arim402@aucklanduni.ac.nz>
2022-08-18 10:11:00 +1200

    Simplified UpdateCountVisit domain class

--- app/src/main/java/com/example/se306_project1/domain/UpdateCountVisit.java
+++ app/src/main/java/com/example/se306_project1/domain/UpdateCountVisit.java
@@ -11,57 +11,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UpdateCountVisit {
+    static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    long currentCount, newCount;
-
-    public void updateCountVisit(Product p){
-        currentCount = getCurrentCount(p);
-
-        newCount = currentCount+1;
-
-        updateCountVisitFireStore(p, newCount);
-    }
-
-    public long getCurrentCount(Product p){
+    public static void updateCountVisit(Product p){
         DocumentReference productRef = db.collection("products").document(""+p.getProductID());
-
-        productRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot snap = task.getResult();
-                    Log.d("firebase", String.valueOf(task.getResult()));
-                    long productCountVisit = (long) snap.get("productCountVisit");
-                    currentCount = productCountVisit;
-                }
-                else{
-                    Log.d("firebase", "error getting data!", task.getException());
-                }
-            }
-        });
-        return currentCount;
+        productRef.update("productCountVisit", FieldValue.increment(1));
     }
 
-    public void updateCountVisitFireStore(Product p, Long newCount){
-        DocumentReference productRef = db.collection("products").document(""+p.getProductID());
-
-        productRef
-                .update("productCountVisit", newCount)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d("Count Visit ", "product " + p.getProductID() + " incremented.");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("Count Visit ", "product " + p.getProductID() + " NOT incremented.");
-                    }
-                });
-    }
 }
