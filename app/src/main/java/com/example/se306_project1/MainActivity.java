003594104aca8d145062b3a84a7c6ef4663a251b
arim402 <arim402@aucklanduni.ac.nz>
2022-08-21 21:56:16 +1200

    Fix search icons temporarily

--- app/src/main/java/com/example/se306_project1/MainActivity.java
+++ app/src/main/java/com/example/se306_project1/MainActivity.java
@@ -53,9 +53,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ProductsDataProvider.addProductsToFirestore();
-        CategoriesDataProvider.addCategoriesToFirestore();
-
         popularList = new ArrayList();
         favouritesList = new ArrayList();
         categoryList = new ArrayList();
