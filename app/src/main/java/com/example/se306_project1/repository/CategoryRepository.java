b021ee1a85fcd08da8092b0ad29d03f958632cdf
samanthamebius <smeb890@aucklanduni.ac.nz>
2022-08-26 11:56:02 +1200

    refactored CategoryRepository

--- app/src/main/java/com/example/se306_project1/repository/CategoryRepository.java
+++ app/src/main/java/com/example/se306_project1/repository/CategoryRepository.java
@@ -1,31 +1,44 @@
 package com.example.se306_project1.repository;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import com.example.se306_project1.models.Category;
 import com.example.se306_project1.models.ICategory;
+import com.example.se306_project1.models.IProduct;
+import com.example.se306_project1.models.Product;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.reflect.TypeToken;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryRepository implements ICategoryRepository{
 
+    private static Context context;
     private List<ICategory> categoryGroups = new ArrayList<>();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference colRef = db.collection("categories");
+
+    public CategoryRepository(Context context){
+        this.context = context;
+    }
 
     // singleton pattern
     private static CategoryRepository instance;
     public static CategoryRepository getInstance(){
         if(instance == null){
-            instance = new CategoryRepository();
+            instance = new CategoryRepository(context);
         }
         return instance;
     }
@@ -35,6 +48,19 @@ public class CategoryRepository implements ICategoryRepository{
         MutableLiveData<List<ICategory>> data = new MutableLiveData<>();
         fetchCategories(data);
         return data;
+    }
+
+    @Override
+    public List<ICategory> getCategoriesCache(String key) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+        List<ICategory> arrayItems = new ArrayList<>();
+        String serializedObject = sharedPreferences.getString(key, null);
+        if (serializedObject != null) {
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<Category>>(){}.getType();
+            arrayItems = gson.fromJson(serializedObject, type);
+        }
+        return arrayItems;
     }
 
     public void fetchCategories(MutableLiveData<List<ICategory>> data){
